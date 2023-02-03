efab33fb692810bc5971c375511b2bbc8785fc12
mohamedderbali <mohamed.derbali@esprit.tn>
2023-02-03 19:53:42 +0100

    search & update

--- routes/contacts.js
+++ routes/contacts.js
@@ -49,4 +49,52 @@ router.get("/updateContact/:contactId", async (req, res, next) => {
     });
   }
 });
+
+router.put("/updateContact/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const { fullName, Phone } = req.body;
+    const updatedContact = {
+      fullName,
+      Phone,
+    };
+    await contactModel.findByIdAndUpdate(contactId, updatedContact);
+    res.redirect("http://localhost:5000/contacts/");
+  } catch (error) {
+    res.render("error", {
+      message: error.message,
+      error: error,
+    });
+  }
+});
+
+router.get("/deleteContact/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    await contactModel.findByIdAndDelete(contactId);
+    res.redirect("http://localhost:5000/contacts/");
+  } catch (error) {
+    res.render("error", {
+      message: error.message,
+      error: error,
+    });
+  }
+});
+router.post("/search", async (req, res, next) => {
+  try {
+    const { search } = req.body;
+    let contactsList = [];
+    if (!search) {
+      contactsList = await contactModel.find();
+    } else {
+      contactsList = await contactModel.find({ fullName: search });
+    }
+    res.render("index", { contactsList });
+  } catch (error) {
+    res.render("error", {
+      message: error.message,
+      error: error,
+    });
+  }
+});
 module.exports = router;
